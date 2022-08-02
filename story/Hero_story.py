7befcdec8183ceb8cf200925bda442c47ce29609
ayan <ayanie@live.co.uk>
2022-08-02 16:10:27 +0100

    hero story

---
+++ story/Hero_story.py
@@ -0,0 +1,19 @@
+# hero story
+
+story1 = {
+    "start":"the hero begins his journey at the centre of space warfare",
+    "middle":"he realises the cause of all the pain and destruction is himself from the future",
+    "end":"to save the 50 billion people in the system he goes to the end of time and seals himself at the edge of the universe",
+    "hero":"star knight" # task 7
+}
+
+print(story1) # printing entire dic
+print(type(story1)) #printing type
+print(story1.keys()) #only keys
+print(story1.values()) #only values
+
+print(story1["start"])
+print(story1["middle"])
+print(story1["end"])
+
+
