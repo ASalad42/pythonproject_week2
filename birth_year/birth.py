c317c1273ea73611b77f024f8820327d6a0116ea
ayan <ayanie@live.co.uk>
2022-08-02 16:33:17 +0100

    birth year

---
+++ birth_year/birth.py
@@ -0,0 +1,13 @@
+# calculate year of birth
+
+name = input("what is your name"+" ")
+age =int(input("age"+" "))
+print("OMG, you are" +" "+str(age)+" "+ "years old and was born in the year...")
+year_born = 2022 - age
+print(year_born)
+
+
+# account for if persons birthday has already happened this year or not
+
+
+# look into library time and print out hours this person has lived for
