c5aebe13e922ea89cd916807ffe4b551f1d6c0cb
ayan <ayanie@live.co.uk>
2022-08-02 11:38:49 +0100

    week2tuesday

---
+++ Collections.py
@@ -0,0 +1,16 @@
+# data collections
+## lists, tuples & dict
+
+### lists
+# what are lists? exam questions related to this
+# very simple sometimes - examples below
+# correct syntax []
+# lists are mutable
+# indexing same concept allies
+
+shopping_list = ["bat", "milk", "bread"]
+            #      0        1       2
+print(shopping_list)
+
+print(type(shopping_list))
+print(len(shopping_list))
--- operators.py
+++ operators.py
@@ -26,22 +26,29 @@ greeting = 'hello world!'
 print(greeting[6:])
 print(greeting[3])
 print(greeting[-9])
-print(greeting[6])
-
-example_string = "jane"
-print(example_string)
-print(len(example_string))
-# strip()
-
-print(len(example_string.strip()))
-#welcome user with their name and welcome message - name and message must start with capital
-example_text = "Heres Some text with lots of text"
-print(example_text.count("text"))
-
-#find a method to bring the statment in captial letter
-print(example_text.capitalize())
-print(example_text.lower())
-
-
+# print(greeting[6])
+#
+# example_string = "jane"
+# print(example_string)
+# print(len(example_string))
+# # strip()
+#
+# print(len(example_string.strip()))
+# #welcome user with their name and welcome message - name and message must start with capital
+# example_text = "Heres Some text with lots of text"
+# print(example_text.count("text"))
+#
+# #find a method to bring the statment in captial letter
+# print(example_text.capitalize())
+# print(example_text.lower())
 
 
+first_name = "james"
+mid_name = "bond"
+last_name = "007"
+age = 47
+address = 33
+postcode = ""
+print(str(age)) #int to string
+print(first_name+" "+mid_name+" "+last_name+" "+str(age)) #simple example of concatenation
+#print(int())
